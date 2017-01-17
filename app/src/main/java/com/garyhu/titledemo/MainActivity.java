33b3c735daeedce7660a483cf461e52fc6fb1fb1
garyhu <hww9081020@163.com>
2017-01-17 17:40:31 +0800

    first commit

---
+++ app/src/main/java/com/garyhu/titledemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.garyhu.titledemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+        setContentView(R.layout.activity_main);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,R.layout.cus_title);
+    }
+}
